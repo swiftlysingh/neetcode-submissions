c2f272069f13d238d742eb892c306b01859e5e08
Pushpinder Pal Singh <sayhi@swiftlysingh.com>
2026-03-30 11:57:09 -0700

    Add: binary-tree-diameter - submission-1

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-1.swift
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func diameterOfBinaryTree(_ root: TreeNode?) -> Int {
+        var maxDia = 0
+
+        func dfs(_ root: TreeNode?) -> Int {
+            guard let root = root else { return 0 }
+
+            var left = dfs(root.left)
+            var right = dfs(root.right)
+
+            maxDia = max(maxDia,left+right)
+
+            return max(left,right) + 1
+        }
+
+        _ = dfs(root)
+        
+        return maxDia
+    }
+}
