da8ed30aed7b24e93ddbdfa1148ca70c287e5f99
Pushpinder Pal Singh <sayhi@swiftlysingh.com>
2026-04-17 17:55:09 -0700

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.swift
@@ -0,0 +1,30 @@
+class Solution {
+    func generateParenthesis(_ n: Int) -> [String] {
+
+        var ans = [Character]()
+        var final = [String]()
+
+        func backtrack(_ left: Int, _ right: Int) {
+            if left == n && right == n {
+                var str = String(ans)
+                final.append(str)
+                return
+            }
+            if left < n {
+                ans.append("(")
+                backtrack(left+1,right)
+                ans.removeLast()
+            }
+
+            if right < left {
+                ans.append(")")
+                backtrack(left,right+1)
+                ans.removeLast()
+            }
+        }
+
+        backtrack(0,0)
+
+        return final
+    }
+}
