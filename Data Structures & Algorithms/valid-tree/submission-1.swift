f348a73fd9ee80d90f7e3b46dbef8505d9365b06
Pushpinder Pal Singh <sayhi@swiftlysingh.com>
2026-04-19 23:33:36 -0700

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.swift
@@ -0,0 +1,44 @@
+class Solution {
+    func validTree(_ n: Int, _ edges: [[Int]]) -> Bool {
+        var adjMap = [Int:[Int]]()
+        var cycle = Set<Int>()
+        var visited = Set<Int>()
+
+        for i in 0..<n {
+            adjMap[i] = []
+        }
+
+        for edj in edges {
+            adjMap[edj[0]]!.append(edj[1])
+            adjMap[edj[1]]!.append(edj[0])
+        }
+
+        func dfs(_ root: Int,_ prev:Int) -> Bool {
+            if cycle.contains(root) { return false }
+            if adjMap[root]!.isEmpty { 
+                visited.insert(root)
+                return true 
+            }
+            
+            cycle.insert(root)
+            for adj in adjMap[root]! {
+                if adj == prev { continue }
+                if !dfs(adj,root) { return false }
+            }
+            visited.insert(root)
+            cycle.remove(root)
+
+            adjMap[root] = []
+
+            return true
+        }
+
+        // for i in 0..<n {
+        //     if !dfs(i) {
+        //         return false
+        //     }
+        // }
+
+        return dfs(0,-1) && visited.count == n
+    }
+}
