c06696b00b31b7054c25655c0819b2e14daee3d7
Pushpinder Pal Singh <sayhi@swiftlysingh.com>
2026-05-03 16:00:34 -0700

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
+        var window = Set<Int>()
+        var left = 0
+
+        for right in 0..<nums.count {
+            if right - left > k {
+                window.remove(nums[left])
+                left += 1
+            }
+
+            if window.contains(nums[right]) { 
+                return true
+            }
+
+            window.insert(nums[right])
+        }
+
+        return false
+    }
+}
