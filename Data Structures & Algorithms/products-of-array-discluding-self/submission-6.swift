b6b0b1bc73f0cf07dd5784493126ebfb8093884f
Pushpinder Pal Singh <sayhi@swiftlysingh.com>
2026-07-29 09:43:52 +0530

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func productExceptSelf(_ nums: [Int]) -> [Int] {
+        var prefix = [Int](repeating: 1, count: nums.count)
+
+        for i in 1..<nums.count {
+            prefix[i] = prefix[i-1] * nums[i-1]
+        }
+
+        var suffix = 1
+        // multiplyin on the fly - O(1) - ignoring output array
+        for i in (0..<nums.count - 1).reversed() {
+            suffix = suffix * nums[i+1]
+            prefix[i] = prefix[i] * suffix
+        }
+
+        return prefix
+    }
+}
